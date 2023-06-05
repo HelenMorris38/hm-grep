f19c1fdd2a69609c1714fa2c6768068c81621cfd
Helen Morris <morris_helen@hotmail.com>
2023-06-05 12:05:51 +0100

    adjusted test

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,5 +1,5 @@
 from src.helpers import load_txt_file
 
 def test_load_txt_file_returns_contents_of_file():
-    output = "Judas Priest\nAC/DC\nBlack Sabbath\nAerosmith\nIron Maiden"
+    output = ['Judas Priest', 'AC/DC', 'Black Sabbath', 'Aerosmith', 'Iron Maiden']
     assert load_txt_file('test-rockbands.txt') == output
