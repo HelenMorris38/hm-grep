81dfea9d1c47e899a5de75819c5d547cd7250f67
Helen Morris <morris_helen@hotmail.com>
2023-06-05 14:02:00 +0100

    add another assert for test case Ju

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,8 @@ from src.main import grep
 
 def test_grep_returns_whole_file_when_passed_emtpy_string():
     output = "Judas Priest\nAC/DC\nBlack Sabbath\nAerosmith\nIron Maiden"
-    assert grep("", 'test-rockbands.txt') == output
+    assert grep("", "test-rockbands.txt") == output
 def test_grep_returns_lines_with_found_string():
     output = "Judas Priest\nBon Jovi\nJunkyard"
-    assert grep("J", 'rockbands.txt') == output
+    assert grep("J", "rockbands.txt") == output
+    assert grep("Ju", "rockbands.txt") == "Judas Priest\nJunkyard"
