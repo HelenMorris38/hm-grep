5be8c92895b44c5dbd9fe6b5b21760af1675707e
Helen Morris <morris_helen@hotmail.com>
2023-06-05 13:59:57 +0100

    grep function works for empty string and a simple search

--- src/main.py
+++ src/main.py
@@ -2,5 +2,10 @@ import re
 from src.helpers import load_txt_file
 
 def grep(strng, filename):
-    return load_txt_file(filename)
+    contents = load_txt_file(filename)
+    result = ""
+    for line in contents:
+        if strng in line:
+            result += line + '\n'
+    return result.rstrip('\n')
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,3 +3,6 @@ from src.main import grep
 def test_grep_returns_whole_file_when_passed_emtpy_string():
     output = "Judas Priest\nAC/DC\nBlack Sabbath\nAerosmith\nIron Maiden"
     assert grep("", 'test-rockbands.txt') == output
+def test_grep_returns_lines_with_found_string():
+    output = "Judas Priest\nBon Jovi\nJunkyard"
+    assert grep("J", 'rockbands.txt') == output
