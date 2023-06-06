fec9e3c1b3d5fcd4fd8af5cd09f4ce69f2c62133
Helen Morris <morris_helen@hotmail.com>
2023-06-06 20:29:55 +0100

    add functionality for -r command

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
 import re
-from src.helpers import load_txt_file
+from src.helpers import load_txt_file, get_file_names, search_file_contents
 
 def grep(strng, filename, command=""):
-    contents = load_txt_file(filename)
-    result = ""
-    for line in contents:
-        if strng in line:
-            result += line + '\n'
-    return result.rstrip('\n')
+    if command == "-r":
+        filenames = get_file_names()
+        result = ""
+        for filename in filenames:
+            contents = load_txt_file(filename)
+            search_results = search_file_contents(strng, contents)
+            if search_results:
+                result += filename + ":" + search_results
+        print(result)
+        return result
+    else:
+        contents = load_txt_file(filename)
+        return search_file_contents(strng, contents)
 
