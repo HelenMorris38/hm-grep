fce6037e8703bc342b41ded8cd12ba021dfad0c7
Helen Morris <morris_helen@hotmail.com>
2023-06-06 16:18:03 +0100

    add helper function get file names

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,18 @@
+from os import listdir, path, walk
+
 def load_txt_file(filename):
     with open(filename,'r') as f:
         lines = [line.rstrip('\n') for line in f]
         return lines
+
+def get_file_names():
+    filenames = []
+    for root, dirs, files in walk("./"):
+        if 'venv' not in root:
+            for file in files:
+                if file.endswith(".txt"):
+                    filenames.append(path.join(root, file))
+    return filenames
+
+
+get_file_names()
