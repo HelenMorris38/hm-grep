bcf850d4857782306818a3c6c519717d70ca0cd4
Helen Morris <morris_helen@hotmail.com>
2023-06-07 18:41:48 +0100

    works for -r functionality

--- src/main.py
+++ src/main.py
@@ -4,20 +4,20 @@ from src.helpers import load_txt_file, get_file_names, search_file_contents
 def grep(strng, filename, command=""):
     if command == "-r":
         filenames = get_file_names()
-        result = ""
+        result = []
+        search_results = []
         for filename in filenames:
             contents = load_txt_file(filename)
-            # search_results = []
-            # for line in contents:
-            #     if 'Nirvana' in line:
-            #         print(line)
-            #     if strng in line:
-            #         search_results.append([filename, line])
-            search_results = search_file_contents(strng, contents)
-            if search_results:
-                result += filename + ":" + search_results +'\n'
-        print(result)
-        return result
+            for line in contents:
+                if strng in line:
+                    search_results.append([filename + ':', line + '\n'])
+            # search_results = search_file_contents(strng, contents)
+
+        print(search_results)
+        results = [''.join(search_result) for search_result in search_results]
+            
+        print(results)
+        return ''.join(results).rstrip('\n')
     else:
         contents = load_txt_file(filename)
         return search_file_contents(strng, contents)
