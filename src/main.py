dfe328ebe5206573786846d9358b20f060b8dfed
Helen Morris <morris_helen@hotmail.com>
2023-06-07 19:41:06 +0100

    remove print statements and white lines

--- src/main.py
+++ src/main.py
@@ -4,19 +4,13 @@ from src.helpers import load_txt_file, get_file_names, search_file_contents
 def grep(strng, filename, command=""):
     if command == "-r":
         filenames = get_file_names()
-        result = []
         search_results = []
         for filename in filenames:
             contents = load_txt_file(filename)
             for line in contents:
                 if strng in line:
                     search_results.append([filename + ':', line + '\n'])
-            # search_results = search_file_contents(strng, contents)
-
-        print(search_results)
         results = [''.join(search_result) for search_result in search_results]
-            
-        print(results)
         return ''.join(results).rstrip('\n')
     else:
         contents = load_txt_file(filename)
