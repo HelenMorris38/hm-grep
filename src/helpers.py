1237b8e0a91d2bd0b425ae9b7e8e26cf92941601
Helen Morris <morris_helen@hotmail.com>
2023-06-16 14:32:20 +0100

    refactor search multiple files

--- src/helpers.py
+++ src/helpers.py
@@ -21,27 +21,12 @@ def search_file(search_term, filename):
         lines = f.readlines()
         result = [line for line in lines if line.find(search_term) != -1]
         return ''.join(result).rstrip('\n')
-    
-# def search_file_contents(strng, contents, invert_match=""):
-#     result = ""
-#     for line in contents:
-#             if strng in line:
-#                 if invert_match:
-#                     if invert_match not in line:
-#                         result += line + '\n'
-#                 else:
-#                     result += line + '\n'
-#     return result.rstrip('\n')
 
-def search_multiple_files(strng, filename, contents, invert_match=""):
-    result = ""
-    for line in contents:
-        if strng in line:
-            if invert_match:
-                if invert_match not in line:
-                    result += filename + ':' + line + '\n'
-            else:
-                result += filename + ':' + line + '\n'
-    return result
-
-
+def search_multiple_files(search_term, filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        result = ""
+        for line in lines:
+            if line.find(search_term) != -1:
+                result += filename + ':' + line
+        return result
--- src/main.py
+++ src/main.py
@@ -1,15 +1,10 @@
 import re
-from src.helpers import load_txt_file, get_file_names, search_file_contents, search_multiple_files, search_file
+from src.helpers import load_txt_file, get_file_names, search_multiple_files, search_file
 
 def grep(search_term="", filename="", command="", invert_match=""):
     if command == "-r":
         filenames = get_file_names()
-        search_results = ""
-        for filename in filenames:
-            contents = load_txt_file(filename)
-            search_result = search_multiple_files(search_term, filename, contents, invert_match)
-            if search_result:
-                search_results += search_result
-        return search_results.rstrip('\n')
+        search_results = [search_multiple_files(search_term, filename) for filename in filenames]
+        return ''.join(search_results).rstrip('\n')
     else:
         return search_file(search_term, filename)
